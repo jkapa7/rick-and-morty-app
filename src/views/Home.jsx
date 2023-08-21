58ce19824e72b0eaa3e4b4de9eb6c07841768738
jkapa7 <juandaniel5005@gmail.com>
2023-08-21 11:55:15 -0500

    Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,17 @@ import Cards from "../components/Cards";
 const Home = () => {
   return (
     <Layout>
-      <button>Characters</button>
+      <section>
+        <h1>Discover the adventures of Rick and Morty</h1>
+
+        <h2>
+          Are you ready to immerse yourself in a world full of craziness, dark
+          humor, and intergalactic adventures? Then do not miss out on the
+          adventures of Rick and Morty.
+        </h2>
+
+        <button>Characters</button>
+      </section>
       <Cards />
     </Layout>
   );
